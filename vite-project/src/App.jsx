f650e6a74b9923aaff463283a2f03fa45b71b447
Zeynep <zarazeynep5858@googlemail.com>
2023-10-06 12:06:03 +0200

    AddNewWhiskeyPage and Navbar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import HomePage from "./pages/HomePage"
-import AllWhiskeysPage from "./pages/AllWhiskeysPage"
+import AddNewWhiskeyPage from "./pages/AddNewWhiskeyPage"
 import WhiskeyDetailsPage from "./pages/WhiskeyDetailsPage"
 import { Link, NavLink, Route, Routes, useParams } from 'react-router-dom'
 import Navbar from "./components/Navbar";
@@ -11,7 +11,7 @@ function App() {
       <h1>Nadia's heaven</h1>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        <Route path='/whiskeys' element={<AllWhiskeysPage />} />
+        <Route path='/whiskeys/new' element={<AddNewWhiskeyPage />} />
         <Route path='/whiskeys/:whiskeyId' element={<WhiskeyDetailsPage />} />
         <Route path='*' element={<h1>404 Page Not Found</h1>} />
       </Routes>
